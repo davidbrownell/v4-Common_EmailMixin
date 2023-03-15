c29c9ea6ef77b6079fe032f519567d6de79b2bde
David Brownell <db@DavidBrownell.com>
2023-03-14 19:52:02 -0700

    🎨 [refactor] Added __version__.py file to python library

---
+++ Libraries/Python/Common_EmailMixin/src/__version__.py
@@ -0,0 +1,18 @@
+# ----------------------------------------------------------------------
+# |
+# |  __version__.py
+# |
+# |  David Brownell <db@DavidBrownell.com>
+# |      2023-03-14 19:49:47
+# |
+# ----------------------------------------------------------------------
+# |
+# |  Copyright David Brownell 2023
+# |  Distributed under the Boost Software License, Version 1.0. See
+# |  accompanying file LICENSE_1_0.txt or copy at
+# |  http://www.boost.org/LICENSE_1_0.txt.
+# |
+# ----------------------------------------------------------------------
+# pylint: disable=invalid-name,missing-module-docstring
+
+VERSION = "0.1.3"
